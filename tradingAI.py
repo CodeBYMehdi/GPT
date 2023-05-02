0f3288f79ddbb639fdb69509a1c48776c62f5e9b
CodeBYMehdi <126424310+CodeBYMehdi@users.noreply.github.com>
2023-05-02 23:26:33 +0100

    Update tradingAI.py

--- tradingAI.py
+++ tradingAI.py
@@ -43,6 +43,7 @@ from scipy.stats import norm
 
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 
 # Import Deep Learning modules
 import keras
@@ -108,290 +109,8 @@ class Market:
 
 
 
-class MACDBbands:
-    def __init__(self):
-        self.data = None
-        self.macd = None
-        self.bbands = None
-        
-    def load_data(self):
+class Balance:
 
-        
-        # initialize MACD and Bollinger Bands
-        self.macd = MACD(self.data['Close'])
-        self.bbands = BollingerBands(self.data['Close'])
-
-        # Error handlin
-        try:
-            self.macd = MACD(self.data['Close'])
-            self.bbands = BollingerBands(self.data['Close'])
-        except ValueError:
-            logging("Error initializing technical indicators. Check your data.")
-            return
-
-    def generate_macd_bbands_signal(self, curr_price):
-        # Get MACD signal
-        macd_signal = self.macd.macd_signal()[-1]
-        macd = self.macd.macd()[-1]
-
-        # Get Bollinger Bands signal
-        upper_band = self.bbands.bollinger_hband()[-1]
-        lower_band = self.bbands.bollinger_lband()[-1]
-
-        if macd_signal > macd and curr_price > upper_band:
-            signal = 'buy_macd'
-        elif macd_signal < macd and curr_price < lower_band:
-            signal = 'sell_macd'
-        else:
-            signal = None
-
-        return signal
-
-
-
-
-
-
-        
-    def run_strategy(self):
-        # use self.macd and self.bbands to make trading decisions
-        pass
-
-
-
-    def cancel_order(self, order_id):
-        order_found = False
-        for order in self.orders:
-            if order.get('id') == order_id:
-                self.orders.remove(order)
-                print(f"Order {order_id} has been cancelled.")
-                order_found = True
-                break
-        if not order_found:
-            print(f"No order found with id {order_id}.")
-
-
-
-
-class TechnicalIndicators:
-    
-    def __init__(self, symbol, timeframe):
-        self.symbol = symbol
-        self.timeframe = timeframe
-        ticker = Ticker(self.symbol)
-        self.data = ticker.history(period=self.timeframe)
-        
-    def volumes(self):
-        return self.data['Volume']
-    
-    def macd(self):
-        return ta.trend.macd_diff(self.data['Close'])
-    
-    def bollinger_bands(self):
-        return ta.volatility.bollinger_hband(self.data['Close']), ta.volatility.bollinger_lband(self.data['Close'])
-
-
-class AlgoTrading():
-    
-    def __init__(self, starting_funds):
-        self.funds = starting_funds
-        self.trade_history = []
-        self.orders={}
-    
-    def algorithmic_strategy(self, prices):
-        ma50 = prices.rolling(window=50).mean()
-        ma200 = prices.rolling(window=200).mean()
-        signals = np.zeros(prices.shape)
-        signals[ma50 > ma200] = 1
-        signals[ma50 < ma200] = -1
-        return signals
-
-    def place_order(self, order_type, units, instrument):
-        data = {
-            "order": {
-                "units": units,
-                "instrument": instrument,
-             "type": order_type,
-                "stopLossOnFill": {
-                    "timeInForce": "GTC",
-                    "price": str(self.stop_loss)
-                },
-             "takeProfitOnFill": {
-                    "timeInForce": "GTC",
-                    "price": str(self.take_profit)
-                 }
-            }
-        }
-        r = self.api.request("POST", "/v3/accounts/" + self.account_id + "/orders", data=data)
-        print(r.status_code, r.reason, r.headers)
-        print(r.data)
-
-    def buy_algo(prices, short_window=10, long_window=30):
-   # Generate a buy signal using a simple moving average crossover strategy
-        short_ma = prices.rolling(window=short_window).mean()
-        long_ma = prices.rolling(window=long_window).mean()
-        crossover = (short_ma > long_ma) & (short_ma.shift(1) < long_ma.shift(1))
-        return crossover.astype(int)
-
-    def sell_algo(prices, short_window=10, long_window=30):
-        #Generate a sell signal using a simple moving average crossover strategy
-        short_ma = prices.rolling(window=short_window).mean()
-        long_ma = prices.rolling(window=long_window).mean()
-        crossover = (short_ma < long_ma) & (short_ma.shift(1) > long_ma.shift(1))
-        return crossover.astype(int)
-
-
-    
-
-
-
-class SupplyAndDemandTrader:
-    
-    def __init__(self, market, fx, stock):
-        self.market = market
-        self.fx = fx
-        self.stock = stock
-        
-    def get_market_demand(self):
-        if self.market == "forex":
-            demand_data = self.fx
-            return demand_data
-        elif self.market == "stocks":
-            demand_data = self.stock['Close']
-            return demand_data
-        else:
-            return {}
-
-    def get_market_supply(self):
-        if self.market == "forex":
-            supply_data = self.fx
-            volume_data = self.fx
-            return supply_data
-        elif self.market == "stocks":
-            supply_data = self.stock['Close']
-            volume_data = self.stock['Volume']
-            return supply_data * volume_data
-        else:
-            return {}
-
-        
-
-
-    def generate_signals(self, prices, supply, demand, trend, stop_loss, take_profit, threshold=0.05):
-        if trend == 'range':
-        # Apply the supply and demand strategy
-            avg_supply = np.convolve(supply, np.ones(10) / 10, mode='valid')
-            avg_demand = np.convolve(demand, np.ones(10) / 10, mode='valid')
-            demand_supply_ratio = avg_demand / avg_supply
-            signals = np.zeros(prices.shape)
-            signals[demand_supply_ratio > 1.05] = 1
-            signals[demand_supply_ratio < 0.95] = -1
-        else:
-        # Apply the algorithmic trading strategy
-            assert len(supply) == len(demand), "Supply and demand arrays must have the same length"
-            buy_signals = np.zeros(len(supply), dtype=bool)
-            sell_signals = np.zeros(len(supply), dtype=bool)
-
-            for i in range(1, len(supply)):
-                supply_ratio = supply[i] / supply[i-1]
-                demand_ratio = demand[i] / demand[i-1]
-                if supply_ratio > (1 + threshold) and demand_ratio < (1 - threshold):
-                # Strong supply increase and demand decrease, signal a sell
-                    sell_signals[i] = True
-                elif supply_ratio < (1 - threshold) and demand_ratio > (1 + threshold):
-                # Strong supply decrease and demand increase, signal a buy
-                    buy_signals[i] = True
-            return buy_signals, sell_signals
-
-
-
-
-
-
-    
-    def generate_positions(self, signals):
-        # implement your position sizing logic here
-        positions = signals.diff()
-        positions.iloc[0] = signals.iloc[0]
-        return positions
-    
-    def calculate_portfolio(self, positions, data):
-        # implement your portfolio calculation logic here
-        portfolio = pd.DataFrame(index=data.index)
-        portfolio['position'] = positions
-        portfolio['price'] = data['close']
-        portfolio['value'] = portfolio['position'] * portfolio['price']
-        portfolio['returns'] = portfolio['value'].pct_change()
-        return portfolio
-    
-    def calculate_returns(self, portfolio):
-        # implement your returns calculation logic here
-        returns = portfolio['returns']
-        returns = returns[~pd.isnull(returns)]
-        return returns
-
-
-
-
-    def generate_prompt(market):
-    
-    # Define prompt prefix based on market
-        if market == "forex":
-            prompt_prefix = "Trade forex based on supply and demand. The current forex market is "
-        elif market == "stocks":
-            prompt_prefix = "Trade stocks based on supply and demand. The current stock market is "
-        else:
-            raise ValueError("Market must be 'forex' or 'stocks'.")
-
-    # Define prompt suffix based on supply and demand in the market
-        
-        demand = get_market_demand(market)
-        supply = get_market_supply(market)
-        
-        if demand > supply:
-            prompt_suffix = "The market is currently in high demand, so look to buy or go long."
-        elif demand < supply:
-            prompt_suffix = "The market is currently oversupplied, so look to sell or go short."
-        else:
-            prompt_suffix = "The market is currently balanced, so look for other opportunities."
-
-    # Combine prompt prefix and suffix
-        prompt = prompt_prefix + prompt_suffix
-
-    # Use GPT-3 API to generate trade prompt
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=60,
-            n=1,
-            stop=None,
-            temperature=0.5,
-            )
-
-        return response.choices[0].text.strip()
-
-    # Define function to make a decision based on input data
-    def make_decision(data):
-    # Define MLPClassifier model with 2 hidden layers of size 10 each
-        clf = MLPClassifier(hidden_layer_sizes=(10, 10), max_iter=1000)
-    
-    # Split data into features (X) and labels (y)
-        X = data[:, :-1]
-        y = data[:, -1]
-
-    
-    # Scale features
-        scaler = StandardScaler()
-        X = scaler.fit_transform(X)
-    
-    # Train the model on the data
-        clf.fit(X, y)
-    
-    # Make a prediction on the last row of data
-        prediction = clf.predict(X[-1].reshape(1, -1))[0]
-    
-    # Return the decision (0 or 1)
-        return prediction
 
     def calculate_units(self, portfolio_value, price):
         # calculate the maximum number of units that can be bought with the portfolio value and the price
@@ -403,7 +122,7 @@ class SupplyAndDemandTrader:
 
     def get_balance(self):
         # implement a function to get the account balance from the IB API
-        # here is an example implementation that assumes that the account currency is USD
+        # here is an example implementation that assumes that the account currency is EUR
         class MyEWrapper(EWrapper):
             def __init__(self):
                 self.account_balance = None
@@ -411,42 +130,68 @@ class SupplyAndDemandTrader:
             def accountSummary(self, reqId, account, tag, value, currency):
                 if tag == "TotalCashValue":
                     self.account_balance = value
+                    
+                    
 
 
 
 
 
-class Backtester:
-    def __init__(self, strategy, data, stock, fx, trend, stop_loss, take_profit):
-        self.strategy = strategy
-        self.data = data
-        self.stock = stock
-        self.fx = fx
-        self.trend = trend
-        self.stop_loss = stop_loss
-        self.take_profit = take_profit
 
-    def load_data(self):
-        if isinstance(self.data, str):
-            if self.data == "msft":
-                msft = yf.Ticker("MSFT")
-                self.data = msft.history(period="max")
-            elif self.data == "fx":
-                fx = ForeignExchange(key=alphavantage_api_key, output_format='pandas')
-                data, meta_data = fx.get_currency_exchange_daily(from_symbol='EUR', to_symbol='USD', outputsize='compact')
-                self.data = data
 
-    def run_backtest(self):
-        self.load_data()
-        sdt = SupplyDemandTrader(self.stock, self.fx)
-        supply_data = sdt.get_supply_data()
-        demand_data = sdt.get_demand_data()
-        signals = self.strategy.generate_signals(self.data, supply_data, demand_data, self.trend, self.stop_loss, self.take_profit)
-        positions = self.strategy.generate_positions(signals)
-        portfolio = self.strategy.calculate_portfolio(positions, self.data)
-        returns = self.strategy.calculate_returns(portfolio)
 
-        return returns
+
+class NNTS:
+    def __init__(self, lookback, units, dropout, epochs, batch_size):
+        self.lookback = lookback
+        self.units = units
+        self.dropout = dropout
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.scaler = MinMaxScaler()
+
+    def _prepare_data(self, data):
+        self.scaler.fit(data)
+        data = self.scaler.transform(data)
+        X, y = [], []
+        for i in range(self.lookback, len(data)):
+            X.append(data[i-self.lookback:i, 0])
+            y.append(data[i, 0])
+        X, y = np.array(X), np.array(y)
+        X = np.reshape(X, (X.shape[0], X.shape[1], 1))
+        
+        return X, y
+
+    def _build_model(self, X):
+        model = Sequential()
+        model.add(LSTM(units=self.units, return_sequences=True, input_shape=(X.shape[1], 1)))
+        model.add(Dropout(self.dropout))
+        model.add(LSTM(units=self.units, return_sequences=True))
+        model.add(Dropout(self.dropout))
+        model.add(LSTM(units=self.units))
+        model.add(Dropout(self.dropout))
+        model.add(Dense(1))
+        model.compile(loss='mean_squared_error', optimizer='adam')
+        
+        return model
+
+    def generate_signals(self, data, strategy):
+        X, y = self._prepare_data(data)
+        model = self._build_model(X)
+        model.fit(X, y, epochs=self.epochs, batch_size=self.batch_size, verbose=0)
+        y_pred = model.predict(X)
+        signals = np.zeros(len(data))
+        signals[self.lookback:] = np.where(y_pred > y, 1, -1)
+        
+        if strategy == 'buy':
+            signals[signals != 1] = 0
+        elif strategy == 'sell':
+            signals[signals != -1] = 0
+        else:
+            signals = np.zeros(len(data))
+        
+        return signals
+
 
 
 
@@ -551,7 +296,7 @@ class PlaceCancelOrder:
         if signal == 'buy':
             self.orders.append({'side': 'buy',
                                 'units': self.units,
-                                'strategy': 'supply_demand',
+                                'strategy': 'NNTS',
                                 'symbol': symbol,
                                 'type': order_type,
                                 'stop_loss': self.stop_loss,
@@ -559,43 +304,12 @@ class PlaceCancelOrder:
         elif signal == 'sell':
             self.orders.append({'side': 'sell',
                                 'units': self.units,
-                                'strategy': 'supply_demand',
+                                'strategy': 'NNTS',
                                 'symbol': symbol,
                                 'type': order_type,
                                 'stop_loss': self.stop_loss,
                                 'take_profit': self.take_profit})
-        elif signal == 'buy_macd':
-            self.orders.append({'side': 'buy',
-                                'units': self.units,
-                                'strategy': 'macd_bbands',
-                                'symbol': symbol,
-                                'type': order_type,
-                                'stop_loss': self.stop_loss,
-                                'take_profit': self.take_profit})
-        elif signal == 'sell_macd':
-            self.orders.append({'side': 'sell',
-                                'units': self.units,
-                                'strategy': 'macd_bbands',
-                                'symbol': symbol,
-                                'type': order_type,
-                                'stop_loss': self.stop_loss,
-                                'take_profit': self.take_profit})
-        elif signal == 'buy_algo':
-            self.orders.append({'side': 'buy',
-                                'units': self.units,
-                                'strategy': 'algo_trading',
-                                'symbol': symbol,
-                                'type': order_type,
-                                'stop_loss': self.stop_loss,
-                                'take_profit': self.take_profit})
-        elif signal == 'sell_algo':
-            self.orders.append({'side': 'sell',
-                                'units': self.units,
-                                'strategy': 'algo_trading',
-                                'symbol': symbol,
-                                'type': order_type,
-                                'stop_loss': self.stop_loss,
-                                'take_profit': self.take_profit})
+
 
     def cancel_order(self, order_id):
         request = orders.OrderCancel(self.account_id, orderID=order_id)
@@ -676,67 +390,56 @@ class Bot:
 
 
 
-# Run the IBapi class
+# Run the loop
 print("test 1")
 bot = Bot()
 print("test 2")    
-fx_price = "fx"
-stock_price = "stock"
+
+# Call Market class
+
 market = Market(symbol='EURUSD', yahoo_ticker='MSFT')
-sdt = SupplyAndDemandTrader(market, fx_price, stock_price)
-supply_data = sdt.get_market_supply()
-demand_data = sdt.get_market_demand()
-    # Run the Market class
 market.fx_price()
 market.stock_price()
 
-    # Run the Backtester class
-backtest = Backtester(sdt, "fx", "stock", supply_data, demand_data, stop_loss, take_profit)
+# Call the Balance class
 
-backtest.run_backtest()
-backtest.preprocess__data()
+balance = Balance()
+balance.calculate_units()
+balance.get_balance()
+balance.accountSummary()
 
-    # Run the Data processor class
-dataprocess = DataProcessor()
-dataprocess.preprocess_data()
+# Call the NNTS class
 
-    # Run the MACDBbands class
-macdbb = MACDBbands()
-macdbb.load_data()
-macdbb.run_strategy()
+nnts = NNTS()
+nnts._prepare_data
+nnts._build_model
+nnts.generate_signals
 
+# Call the DataProcessor class
 
-    # Run the Technical Indicators class
-indicators = TechnicalIndicators()
-indicators.volumes()
-indicators.macd()
-indicators.bollinger_bands()
+datapp = DataProcessor()
+datapp.preprocess_data
 
-    # Run the Algo trading class
-algotrading = AlgoTrading()
-algotrading.algorithmic_strategy()
+# Call the RiskManager class
 
+riskmg = RiskManager()
+riskmg.update_equity
+riskmg.calculate_risk
+riskmg.can_open_position
+riskmg.can_afford_position
+riskmg.open_position
+riskmg.close_position
+riskmg.update_position
 
-    # Run the Supply and Demand class
-supplydemand = SupplyAndDemandTrader()
-supplydemand.get_market_demand()
-supplydemand.get_market_supply()
-supplydemand.generate_signals()
-supplydemand.generate_positions()
-supplydemand.calculate_portfolio()
-supplydemand.calculate_returns()
-supplydemand.generate_prompt()
-supplydemand.make_decision()
-supplydemand.get_balance()
+# Call PlaceCancelOrder class
 
+pcorder = PlaceCancelOrder()
+pcorder.place_order
+pcorder.cancel_order
 
-    # Run the Risk manager class
-riskmanagement = RiskManager()
-riskmanagement.update_equity()
-riskmanagement.calculate_risk()
-riskmanagement.can_open_position()
-riskmanagement.can_afford_position()
-riskmanagement.open_position()
-riskmanagement.close_position()
-riskmanagement.update_position()
-app.run()
+# Call the Bot class
+
+bot = Bot()
+bot.execute_trade
+bot.run_loop
+app.run
